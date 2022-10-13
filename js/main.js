dc1bc11accf5b9cb94fca565ab4736404fd230bb
juanisuarez28 <juanisua3@gmail.com>
2022-10-12 21:21:48 -0300

    funcion de carrusel agregada

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+"use strict"
+
 progress();
 function progress(){
     let porcentaje = document.querySelector('.porcentaje');
@@ -16,3 +18,46 @@ function progress(){
   function closeLoader(){
     document.getElementById("loader").classList.toggle("loader2");
 }
+
+const cards = document.getElementsByClassName('.cards');
+
+let i=0;
+
+const btnDerecha =document.getElementsByClassName('.btn-derecha');
+btnDerecha.addEventListener('click', function(){
+  
+  if(i==0){
+    i++;
+    cards.style.transform =`translateX(${-25}%)`;
+  }else if(i==1){
+    i++;
+    cards.style.transform =`translateX(${-50}%)`;
+  }else if(i==2){
+    i++;
+    cards.style.transform =`translateX(${-75}%)`;
+  }else{
+    i=0;
+    cards.style.transform =`translateX(${-0}%)`
+  }
+  
+})
+
+
+const btnIzquierda =document.getElementsByClassName('.btn-izquierda');
+btnIzquierda.addEventListener('click', function(){
+  
+  if(i==0){
+    i=3;
+    cards.style.transform =`translateX(${-75}%)`;
+  }else if(i==3){
+    i=2;
+    cards.style.transform =`translateX(${-50}%)`;
+  }else if(i==2){
+    i=1
+    cards.style.transform =`translateX(${-25}%)`;
+  }else{
+    i=0;
+    cards.style.transform =`translateX(${-0}%)`
+  }
+  
+})
