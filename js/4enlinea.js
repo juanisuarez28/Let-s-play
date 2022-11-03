669e48583cde544e76200e0a1527ff954b893ab4
juanisuarez28 <juanisua3@gmail.com>
2022-11-03 17:55:37 -0300

    imagenes de zombies y plantas agregadas

--- js/4enlinea.js
+++ js/4enlinea.js
@@ -2,136 +2,187 @@
 
 document.addEventListener("DOMContentLoaded", function () {
 
-//MAIN DEL JUEGO
-//SE PROCEDE A AGARRAR TODAS LAS VARIABLES; TANTO OPCIONES DE JUEGO, FICHAS, ETC
+    //MAIN DEL JUEGO
+    //SE PROCEDE A AGARRAR TODAS LAS VARIABLES; TANTO OPCIONES DE JUEGO, FICHAS, ETC
 
 
-let canvas=document.getElementById('canvas');
+    let canvas = document.getElementById('canvas');
 
- /** @type {CanvasRenderingContext2D} */
-let ctx=canvas.getContext('2d');
-let board = new Tablero(canvas,0,0,null);
-let fichas=[];
-
-
-
-
-
-//por ahora se ejecuta sola, despues se ejecutaria con el botonJugar
-function init() {
-    createBoard();
-    addFicha();
-    dibujarFichas();
-    initEvents();
-    setInterval(dibujarFichas, 20);
-    
-}
+    /** @type {CanvasRenderingContext2D} */
+    let ctx = canvas.getContext('2d');
+    let board = new Tablero(canvas, 0, 0, null);
+    let fichas = [];
+    let jugador1 = "jugador1";
+    let jugador2 = "jugador2";
 
 
-//EVENTOS DE MOVIMIENTO
-function initEvents(){
-    canvas.onmousedown=mouseDown;
-    canvas.onmousemove=mouseMove;
-    canvas.onmouseup=mouseUp;
-}
 
-function mouseDown(event){
-    let x=event.clientX - event.currentTarget.offsetLeft;
-    let y= event.clientY - event.currentTarget.offsetTop;
 
-    fichas.forEach(ficha => {
-        if(ficha.checkSelected(x, y)){
-            console.log("seleccionada")
-            ficha.setSelected(true);
-        }else{
+    //por ahora se ejecuta sola, despues se ejecutaria con el botonJugar
+    function init() {
+        createBoard();
+        addFicha();
+        dibujarFichas();
+        initEvents();
+        setInterval(dibujarFichas, 20);
+
+    }
+
+
+    //EVENTOS DE MOVIMIENTO
+    function initEvents() {
+        canvas.onmousedown = mouseDown;
+        canvas.onmousemove = mouseMove;
+        canvas.onmouseup = mouseUp;
+    }
+
+    function mouseDown(event) {
+        let x = event.clientX - event.currentTarget.offsetLeft;
+        let y = event.clientY - event.currentTarget.offsetTop;
+        
+        fichas.forEach(ficha => {
+            if (ficha.checkSelected(x, y)) {
+                console.log("seleccionada")
+                ficha.setSelected(true);
+            } else {
+                ficha.setSelected(false);
+            }
+        });
+    }
+
+
+    function mouseMove(event) {
+        let x = event.clientX - event.currentTarget.offsetLeft;
+        let y = event.clientY - event.currentTarget.offsetTop;
+
+        fichas.forEach(ficha => {
+            if (ficha.isSelected()) {
+                ficha.move(x, y);
+            }
+        });
+    }
+
+    function mouseUp() {
+        fichas.forEach(ficha => {
             ficha.setSelected(false);
-        }
-    });
-}
+        });
 
-
-function mouseMove(event) {
-    let x=event.clientX - event.currentTarget.offsetLeft;
-    let y= event.clientY - event.currentTarget.offsetTop;
-    
-    fichas.forEach(ficha => {
-        if(ficha.isSelected()){
-            ficha.move(x,y); 
-        }
-    });
-}
-
-function mouseUp() {
-    fichas.forEach(ficha => {
-        ficha.setSelected(false);    
-    });
-    
-}
-
-
-
-//Inicia el juego y se crea el tablero
-
-
-function createBoard(){
-    board.clearBoard(); //limpia tablero
-    let goal = parseInt(document.getElementById("selectJuego").value); //toma la configuracion del modo de juego
-    let boardStyle = document.getElementById("selectBoard").value; //toma la configuracion del estilo de tablero
-    board = new Tablero(canvas,goal,boardStyle,ctx); //instancia un tablero nuevo
-    board.create(); //crea las casillas
-    board.drawBoard(); //dibuja las casillas
-}
-
-
-//Una vez iniciado el juego se pushean las fichas(segun modo de juego se crean x fichas)
-let goalConfig = parseInt(document.getElementById("selectJuego").value);
-function addFicha(){
-    let cantFichas = parseInt(goalConfig)+38
-    let yAxis = 30;
-    let mitad = Math.floor(cantFichas / 2);
-    
-    for(let i = 0; fichas.length<=mitad; i++){
-        let ficha = new Ficha (40, yAxis, ctx,"blue","player1");
-        fichas.push(ficha);
-        yAxis += 30;
     }
-    yAxis = 30;
-    for(let i = 0; fichas.length<=cantFichas; i++){
-        let ficha = new Ficha (1000, yAxis, ctx,"red","player2");
-        fichas.push(ficha);
-        yAxis += 30;
+
+
+
+    //Inicia el juego y se crea el tablero
+
+
+    function createBoard() {
+        board.clearBoard(); //limpia tablero
+        let goal = parseInt(document.getElementById("selectJuego").value); //toma la configuracion del modo de juego
+        let boardStyle = document.getElementById("selectBoard").value; //toma la configuracion del estilo de tablero
+        board = new Tablero(canvas, goal, boardStyle, ctx); //instancia un tablero nuevo
+        board.create(); //crea las casillas
+        board.drawBoard(); //dibuja las casillas
     }
-}
+
+
+    //Una vez iniciado el juego se pushean las fichas(segun modo de juego se crean x fichas)
+    function addFicha() {
+        let rows = parseInt(board.getRows())-1 ;
+        let columns = parseInt(board.getColumns());
+        let cantFichas = rows *  columns;
+        let yAxis = 30;
+        let mitad = Math.floor(cantFichas / 2);
+        let mitadDeMitadPrimera = (mitad-Math.floor(mitad/2));
+        let mitadDeMitadSegunda = (cantFichas-Math.floor(mitad/2));
+
+        fichas = [];
+        let estiloFichas = document.getElementById("selectRivales").value;
+        let estilo = null;
+        if (estiloFichas == "plantas"){
+            estilo = {
+                j1:"img/planta.png",
+                j2:"img/zombie.png"
+            };
+        }else if(estiloFichas== "girasoles"){
+            estilo = {
+                j1:"img/girasol.png",
+                j2:"img/soldado.png"
+            };
+        }
+        for (let i = 0; i < mitad; i++) {
+            if(i<mitadDeMitadPrimera){
+                let ficha = new Ficha(40, yAxis, ctx, "jugador1", estilo);
+                fichas.push(ficha);
+                yAxis += 30;
+                if(i==mitadDeMitadPrimera-1){
+                    yAxis = 30;
+                }
+            }
+            else{
+                let ficha = new Ficha(100, yAxis, ctx, "jugador1", estilo);
+                fichas.push(ficha);
+                yAxis += 30;
+            }
+        }
+        yAxis = 30;
+        for (let i = mitad; i < cantFichas; i++) {
+            if(i<mitadDeMitadSegunda){
+                let ficha = new Ficha(940, yAxis, ctx, "jugador2", estilo);
+                fichas.push(ficha);
+                yAxis += 30;
+                if(i==mitadDeMitadSegunda-1){
+                    yAxis = 30;
+                }
+            }
+            else{
+                let ficha = new Ficha(1000, yAxis, ctx, "jugador2", estilo);
+                fichas.push(ficha);
+                yAxis += 30;
+            }
+        }
+        
+    }
 
 
 
 
 
-//antes de dibujarlas a la hora de mover las fichas, se borra el registro con la funcion clearCanvas() para simular el desplazamiento
-function dibujarFichas(){
-    clearCanvas();
-    board.drawBoard();
-    fichas.forEach(ficha => {
-        ficha.draw();
+    //antes de dibujarlas a la hora de mover las fichas, se borra el registro con la funcion clearCanvas() para simular el desplazamiento
+    function dibujarFichas() {
+        clearCanvas();
+        board.drawBoard();
+        fichas.forEach(ficha => {
+            ficha.draw();
+        })
+    }
+
+    //Limpieza de canvas, restauracion de tablero
+    function clearCanvas() {
+        board.clearBoard;   //limpia tablero
+        ctx.clearRect(0, 0, 1080, 700);
+    }
+
+
+
+
+
+
+    // INICIA EL JUEGO
+    document.getElementById("jugar").addEventListener("click", () => {
+        clearCanvas();
+        init();
     })
-}
 
-//Limpieza de canvas, restauracion de tablero
-function clearCanvas() {
-    board.clearBoard;   //limpia tablero
-    ctx.clearRect(0, 0, 1080, 700);
-}
-
-
-
-
-
-
-// INICIA EL JUEGO
-document.getElementById("jugar").addEventListener("click", ()=>{
-    clearCanvas();
-    init(); 
-})
-
+    /*
+    
+    //VERIFICAR GANADOR
+    function verificarGanador(){
+        if(board.verify(jugador1)){
+            return "gano el jugador 1";
+        }else if(board.verify(jugador2)){
+            return "gano el jugador 2";
+        }
+        
+    }
+*/
 
 });
