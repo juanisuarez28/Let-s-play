f261bdeb3ba7c68e664ec33a5e798bf1b5a227f3
juanisuarez28 <juanisua3@gmail.com>
2022-11-22 18:01:38 -0300

    punto 1 cambiado y terminado

--- js/animations.js
+++ js/animations.js
@@ -1,20 +1,30 @@
 "use strict"
 
-let labelHambur = document.querySelector("#menu-hambur-label");
-let hambur = labelHambur.querySelector(".menu-hambur");
-let cross = labelHambur.querySelector(".menu-cross");
 
-labelHambur.addEventListener("click", () => {
-    hambur.style.animationName = "none";
-    cross.style.animationName = "none";
-    
-    setTimeout(() => {
-        hambur.style.animationName = "hamburgTransition";
-        cross.style.animationName = "crossAppear";
-    }, 0);
+//Funcion menu hamburguesa icono cruz
+let burguer=document.querySelector('#burguer');
+
+burguer.addEventListener('click', e=>{
+    burguer.classList.toggle('active');
 })
 
+// let labelHambur = document.querySelector("#menu-hambur-label");
+// let hambur = document.querySelector(".menu-hambur");
+// let cross = document.querySelector(".menu-cross");
 
+// labelHambur.addEventListener("click", () => {
+//     hambur.style.animationName = "none";
+//     cross.style.animationName = "none";
+    
+//     setTimeout(() => {
+//         hambur.style.animationName = "hamburgTransition";
+//         cross.style.animationName = "crossAppear";
+//     }, 0);
+// })
+
+
+
+//Funcion header sticky
 window.onscroll = function() {stickyHeader()};
 
 var header = document.querySelector(".header");
