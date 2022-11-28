31bd63b9c458133e4113de4740d5c3737fa2361d
juanisuarez28 <juanisua3@gmail.com>
2022-11-28 16:25:17 -0300

    punto 4 terminado

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,54 +1,60 @@
 "use strict"
-
+//CARRUSEL CON ANIMACION
 
 
 //funcion para botones derecha en carrusel
 let i=0;
-let btnsDerecha =document.querySelectorAll('.btn-derecha');
+let btnsDerecha =document.querySelector('.btn-derecha');
 
-for (let a = 0; a < btnsDerecha.length; a++) {
-  let element = btnsDerecha[a];  
-  element.addEventListener("click", ()=> {
+let cards=document.querySelectorAll(".card");
+  
+btnsDerecha.addEventListener("click", ()=> {
+
+  cards.forEach(card => {
+    card.classList.remove("animacionCards");
+  });
 
     if(i==0){
       i++;
-      element.nextElementSibling.style.transform =`translateX(${-25}%)`;
+      btnsDerecha.nextElementSibling.style.transform =`translateX(${-50}%)`;
+      cards[4].classList.add("animacionCards");
+      cards[5].classList.add("animacionCards");
+      cards[6].classList.add("animacionCards");
+      cards[7].classList.add("animacionCards");
     }else if(i==1){
-      i++;
-      element.nextElementSibling.style.transform =`translateX(${-50}%)`;
-    }else if(i==2){
-      i++;
-      element.nextElementSibling.style.transform =`translateX(${-75}%)`;
-    }else{
-      i=0;
-      element.nextElementSibling.style.transform =`translateX(${-0}%)`
+      i--;
+      btnsDerecha.nextElementSibling.style.transform =`translateX(${-0}%)`;
+      cards[0].classList.add("animacionCards");
+      cards[1].classList.add("animacionCards");
+      cards[2].classList.add("animacionCards");
+      cards[3].classList.add("animacionCards");
     }
     }
-  );
-}
-
+);
 
 
 //funcion para botones izquierda carrusel
-let btnsIzquierda =document.querySelectorAll('.btn-izquierda');
+let btnsIzquierda =document.querySelector('.btn-izquierda');
+  btnsIzquierda.addEventListener("click", ()=> {
 
-for (let a = 0; a < btnsIzquierda.length; a++) {
-  let element = btnsIzquierda[a];  
-  element.addEventListener("click", ()=> {
-
+    cards.forEach(card => {
+      card.classList.remove("animacionCards");
+    });
     if(i==0){
-      i=3;     
-      element.nextElementSibling.nextElementSibling.style.transform =`translateX(${-75}%)`;
-    }else if(i==3){
-        i=2;
-        element.nextElementSibling.nextElementSibling.style.transform =`translateX(${-50}%)`;
-    }else if(i==2){
-        i=1
-        element.nextElementSibling.nextElementSibling.style.transform =`translateX(${-25}%)`;
-    }else{
+      i=1;     
+      btnsIzquierda.nextElementSibling.nextElementSibling.style.transform =`translateX(${-50}%)`;
+      cards[4].classList.add("animacionCards");
+      cards[5].classList.add("animacionCards");
+      cards[6].classList.add("animacionCards");
+      cards[7].classList.add("animacionCards");
+    }else if(i==1){
         i=0;
-        element.nextElementSibling.nextElementSibling.style.transform =`translateX(${-0}%)`
+        btnsIzquierda.nextElementSibling.nextElementSibling.style.transform =`translateX(${-0}%)`;
+        cards[0].classList.add("animacionCards");
+        cards[1].classList.add("animacionCards");
+        cards[2].classList.add("animacionCards");
+        cards[3].classList.add("animacionCards");
     }
   }
   );
-}
+
