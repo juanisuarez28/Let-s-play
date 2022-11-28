5287e526302c27715e8d660c803812b771669e47
juanisuarez28 <juanisua3@gmail.com>
2022-11-27 22:35:39 -0300

    punto 7 terminado

--- js/animations.js
+++ js/animations.js
@@ -62,3 +62,73 @@ function mostrarAlScrollear(){
   };
 
 }
+
+
+
+//FUNCION ANIMACION HISTORIA
+
+let textosAnimados = document.querySelectorAll(".appear");
+let buttonUp = document.querySelector(".buttonUp");
+let buttonDown = document.querySelector(".buttonDown");
+let count = 0;
+
+buttonUp.addEventListener("click", ()=>{
+  let texto;
+  if(count>0){
+    count = count -1;
+  }
+  
+  textosAnimados.forEach(text => {
+    text.classList.remove("animarScroll");
+    text.classList.add("desanimarScroll");
+  });
+
+  switch(count){
+    case 0:
+      texto = textosAnimados[0];
+      texto.classList.remove("desanimarScroll");
+      texto.classList.add("animarScroll");
+      break;
+    case 1:
+      texto = textosAnimados[1];
+      texto.classList.remove("desanimarScroll");
+      texto.classList.add("animarScroll");
+      break;
+    case 2:
+      texto = textosAnimados[2];
+      texto.classList.remove("desanimarScroll");
+      texto.classList.add("animarScroll");
+      break;
+  }
+})
+
+buttonDown.addEventListener("click", ()=>{
+  let texto;
+
+  if(count<2){
+    count = count +1;
+  }
+
+  textosAnimados.forEach(text => {
+    text.classList.remove("animarScroll");
+    text.classList.add("desanimarScroll");
+  });
+
+  switch(count){
+    case 0:
+      texto = textosAnimados[0];
+      texto.classList.remove("desanimarScroll");
+      texto.classList.add("animarScroll");
+      break;
+    case 1:
+      texto = textosAnimados[1];
+      texto.classList.remove("desanimarScroll");
+      texto.classList.add("animarScroll");
+      break;
+    case 2:
+      texto = textosAnimados[2];
+      texto.classList.remove("desanimarScroll");
+      texto.classList.add("animarScroll");
+      break;
+  }
+})
